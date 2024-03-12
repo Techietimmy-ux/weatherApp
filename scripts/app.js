931d0c70b0f82baf2cc27c3f7b83e7bcfc66dd0f
techietimmy-ux <ikpediatimmy@gmail.com>
2024-03-12 07:39:58 +0100

    made a weather app

---
+++ scripts/app.js
@@ -0,0 +1,75 @@
+const form = document.querySelector('form')
+const cardDetails = document.querySelector('.details')
+const card = document.querySelector('.card')
+const dayImage = document.querySelector('img.time')
+const iconImage = document.querySelector('.icon img')
+
+
+
+
+
+
+
+//getting user input
+form.addEventListener('submit', e => {
+
+    e.preventDefault()
+
+    const cityName = form.city.value.trim()
+
+
+
+    updateCity(cityName).then(data => {
+        console.log(data);
+        updateUI(data)
+    }).catch(err => console.log(err))
+
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none')
+    }
+
+    form.reset()
+})
+
+//updating city
+const updateCity = async (cityName) => {
+    const cityData = await getCity(cityName)
+    const weather = await getWeather(cityData.Key)
+
+    return { cityData, weather }
+}
+
+
+// function for updating ui
+const updateUI = (data) => {
+
+    const cityData = data.cityData
+    const weather = data.weather
+
+    cardDetails.innerHTML = `
+    <div class="text-muted text-uppercase text-center details">
+                <h5 class="my-3">${cityData.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;c</span>
+                </div>
+            </div>
+    `
+
+    let daySRC;
+
+    if (weather.IsDayTime === true) {
+        daySRC = 'img/day.svg'
+    } else {
+        daySRC = 'img/night.svg'
+    }
+    dayImage.setAttribute('src', daySRC)
+
+    const iconSRC = `img/icons/${weather.WeatherIcon}.svg`
+
+    iconImage.setAttribute('src', iconSRC)
+
+
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const APIkey = 'dgpnRTOift02lk1Poe11C4Svp28njXdJ'
+
+
+
+
+
+
+// getCity information
+const getCity = async (cityName) => {
+
+    const resourceURL = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=%09${APIkey}&q=${cityName}`
+
+
+    const response = await fetch(resourceURL + query)
+    const data = await response.json()
+    return data[0]
+
+
+}
+
+
+
+//get weather information
+const getWeather =async(cityKey)=>{
+    const resourceURL =`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${APIkey}`
+
+    const response =await fetch(resourceURL)
+    const data = await response.json()
+
+    return data[0]
+
+}
